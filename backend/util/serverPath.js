79f0e281757a6c4c7dcee8789d048b65e96dc2c3
techsamuelsani <70712740+techsamuelsani@users.noreply.github.com>
2022-07-04 14:22:02 +0600

    Update serverPath.js

--- backend/util/serverPath.js
+++ backend/util/serverPath.js
@@ -1,3 +1,3 @@
-exports.serverPath = "http://localhost:5000"; // example: https://example.example.com/
+exports.serverPath = "http://147.182.179.140:5000"; // example: https://example.example.com/
 
 exports.serverKey = "AAAAdpcBhsI:APA91bEznsvZ5Ev9xigNHPuRT-Sixblr-sQMKfyoUmNAgKpiqgts2xeYEO_V4IplfAMkXX6dYoKvy_PWFO-2_XYtZB97DOkI01EiZKdNPTyHQj_ZR3pel6qrYdgGOv0HlgDwxfjgy0wh"; //get this from firebase
